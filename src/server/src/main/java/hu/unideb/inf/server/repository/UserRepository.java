3adf923c78c3766dd408b09fbb6fac01e4726ba3
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-09-30 19:23:11 +0200

    add: user repository

---
+++ src/server/src/main/java/hu/unideb/inf/server/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package hu.unideb.inf.server.repository;
+
+import hu.unideb.inf.server.model.base.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
