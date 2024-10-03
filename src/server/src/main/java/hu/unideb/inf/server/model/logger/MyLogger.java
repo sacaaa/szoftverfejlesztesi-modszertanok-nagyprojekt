69be3697e2a016b99b996bed43da459a738cd7f3
Sándor Székelyhidi <szekelyhidis@mailbox.unideb.hu>
2024-10-03 14:26:11 +0200

    add: logoló osztály

---
+++ src/server/src/main/java/hu/unideb/inf/server/model/logger/MyLogger.java
@@ -0,0 +1,10 @@
+package hu.unideb.inf.server.model.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyLogger {
+
+    public static final Logger log = LoggerFactory.getLogger(MyLogger.class);
+
+}
