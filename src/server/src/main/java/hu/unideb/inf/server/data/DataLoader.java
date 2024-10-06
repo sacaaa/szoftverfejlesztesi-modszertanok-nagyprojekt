47370d8de19698db55642a845843d2662fb325f4
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-10-06 21:14:32 +0200

    edit: új adatok betöltése

--- src/server/src/main/java/hu/unideb/inf/server/data/DataLoader.java
+++ src/server/src/main/java/hu/unideb/inf/server/data/DataLoader.java
@@ -1,16 +1,15 @@
 package hu.unideb.inf.server.data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hu.unideb.inf.server.model.Review;
 import hu.unideb.inf.server.model.Subject;
 import hu.unideb.inf.server.model.Teacher;
+import hu.unideb.inf.server.model.TeacherSubjectAtSchool;
 import hu.unideb.inf.server.model.base.User;
 import hu.unideb.inf.server.model.logger.MyLogger;
 import hu.unideb.inf.server.model.users.School;
 import hu.unideb.inf.server.model.users.Student;
-import hu.unideb.inf.server.repository.SchoolRepository;
-import hu.unideb.inf.server.repository.StudentRepository;
-import hu.unideb.inf.server.repository.SubjectRepository;
-import hu.unideb.inf.server.repository.TeacherRepository;
+import hu.unideb.inf.server.repository.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -46,12 +45,20 @@ public class DataLoader {
     @Autowired
     private TeacherRepository teacherRepository;
 
+    @Autowired
+    private TeacherSubjectAtSchoolRepository teacherSubjectAtSchoolRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
     @PostConstruct
     public void load() {
         loadData("classpath:data/schools.json", School[].class, schoolRepository);
         loadData("classpath:data/students.json", Student[].class, studentRepository);
         loadData("classpath:data/subjects.json", Subject[].class, subjectRepository);
         loadData("classpath:data/teachers.json", Teacher[].class, teacherRepository);
+        loadData("classpath:data/teacher_subject_at_schools.json", TeacherSubjectAtSchool[].class, teacherSubjectAtSchoolRepository);
+        loadData("classpath:data/reviews.json", Review[].class, reviewRepository);
     }
 
     private <T> void loadData(String resourcePath, Class<T[]> clazz, JpaRepository<T, Long> repository) {
