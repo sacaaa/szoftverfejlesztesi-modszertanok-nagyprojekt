d7c9270ac660afdb18f7cd572b6b61c993d8a8fa
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-10-07 21:31:45 +0200

    edit: TeacherSubhectAtSchool api végpont átnevezés

--- src/server/src/main/java/hu/unideb/inf/server/controller/TeacherSubjectAtSchoolController.java
+++ src/server/src/main/java/hu/unideb/inf/server/controller/TeacherSubjectAtSchoolController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/teacher-subject-at-school")
+@RequestMapping("/api/teacher-subject-at-schools")
 public class TeacherSubjectAtSchoolController {
 
     @Autowired
