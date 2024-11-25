4c4c9d7cd58b69c538f8b2d1c439c4f21a083182
Tóth Dániel <tothdani561@gmail.com>
2024-11-25 16:58:19 +0100

    Delete useAuth and authentication

--- src/client/src/assets/components/UserProfile/jwtDecode.ts
+++
@@ -1,9 +0,0 @@
-export const decodeToken = (token: string): { email?: string } | null => {
-    try {
-        const payload = JSON.parse(atob(token.split(".")[1])); // Középső rész a payload
-        return payload;
-    } catch (error) {
-        console.error("[decodeToken] Failed to decode token:", error);
-        return null;
-    }
-};
--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './useAuth.tsx'
 import App from './App.tsx'
 import './index.css'
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>,
 )
