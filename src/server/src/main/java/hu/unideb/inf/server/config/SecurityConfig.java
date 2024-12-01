90747cf5637e074e3bd2de56440b3177a20cdb35
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-12-01 09:51:03 +0100

    edit: backend adat lekérdezés

--- src/server/src/main/java/hu/unideb/inf/server/config/SecurityConfig.java
+++ src/server/src/main/java/hu/unideb/inf/server/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers("api/data/**").permitAll()
                         .anyRequest().authenticated())
                 .cors(Customizer.withDefaults())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT ellenőrzés;
---
+++ src/server/src/main/java/hu/unideb/inf/server/controller/DataController.java
@@ -0,0 +1,49 @@
+package hu.unideb.inf.server.controller;
+
+import hu.unideb.inf.server.service.impl.ReviewService;
+import hu.unideb.inf.server.service.impl.SchoolService;
+import hu.unideb.inf.server.service.impl.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/data")
+public class DataController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @Autowired
+    private SchoolService schoolService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> getData() {
+        var teacherCount = teacherService.getAllTeachers().size();
+        var reviewCount = reviewService.getAllReviews().size();
+        var schoolCount = schoolService.getAllSchools().size();
+
+        return ResponseEntity.ok(Map.of(
+                "teacherCount", formatCount(teacherCount),
+                "reviewCount", formatCount(reviewCount),
+                "schoolCount", formatCount(schoolCount)
+        ));
+    }
+
+    private String formatCount(int count) {
+        if (count < 10) {
+            return String.valueOf(count);
+        }
+        int magnitude = (int) Math.pow(10, (int) Math.log10(count));
+        int roundedValue = (count / magnitude) * magnitude;
+        return roundedValue + "+";
+    }
+
+}
